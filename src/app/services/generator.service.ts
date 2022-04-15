f32fc6cccf38532430e2c07e659314f36c0022cf
Joel Sahli <joel.sahli@outlook.com>
2022-04-16 01:37:51 +0200

    generator service produces random number and does single api request

--- src/app/services/generator.service.ts
+++ src/app/services/generator.service.ts
@@ -5,11 +5,22 @@ import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class GeneratorService {
-    constructor(private http: HttpClient ) {}
+  constructor(private http: HttpClient) { }
 
-    pokeApi = 'http://localhost/pokemon'
+  pokeApi = 'http://localhost/pokemon'
 
-    getPokemon() {
-        return this.http.get<any>(this.pokeApi);
-      }
+  getPokemon() {
+    var arr = [];
+    while (arr.length < 6) {
+      var r = Math.floor(Math.random() * 898) + 1;
+      if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    console.log(arr);
+
+
+
+    var pokemon1 = arr[0]
+
+    return this.http.get<any>(this.pokeApi +'/'+ pokemon1);
+  }
 }
